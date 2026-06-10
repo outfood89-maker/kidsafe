090f7fe9ef020e0c62c45cd18f59a54c4d3853a9
Freddie <kimkimbap@naver.com>
2026-06-10 15:52:58 +0900

    fix: 프로필 아바타 인물 위치 추가 하향 조정

--- client/src/pages/ProfileSelect.jsx
+++ client/src/pages/ProfileSelect.jsx
@@ -178,9 +178,9 @@ export default function ProfileSelect() {
                           width: "100%",
                           height: "100%",
                           objectFit: "cover",
-                          objectPosition: "center 15%",
+                          objectPosition: "center 30%",
                           transform: "scale(1.35)",
-                          transformOrigin: "center 27%",
+                          transformOrigin: "center 45%",
                         }}
                       />
                     </div>
